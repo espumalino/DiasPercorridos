64abfd44057b13ef16aca0a90c194673253e8160
celso.alunos <joaopedro.dancam@gmail.com>
2024-06-28 15:59:07 -0300

    FINALIZADO

---
+++ src/DiasVividos.java
@@ -0,0 +1,12 @@
+public class DiasVividos {
+
+    public static void main(String[] args) {
+
+        byte personAge = 18;
+        final short DAYS_IN_A_FISCAL_YEAR = 365;
+        int daysLived = personAge * DAYS_IN_A_FISCAL_YEAR;
+
+        System.out.println("Você já viveu aproximadamente " + daysLived + " dias!");
+
+    }
+}
